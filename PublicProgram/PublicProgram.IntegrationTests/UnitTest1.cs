cf2685b92f96f62be49bb05bdb08bad4d601428e
Roman Kuzmych <roman.kuzmich@gmail.com>
2024-06-08 16:35:53 +0300

    initial-create

---
+++ PublicProgram/PublicProgram.IntegrationTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace PublicProgram.IntegrationTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
